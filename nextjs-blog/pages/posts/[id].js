8b3e47c9e1cec35164518bcd13ebf1e759f66954
Kisan Pandya <kisanapandya1@gmail.com>
2023-11-30 19:07:18 -0500

    new

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,9 +1,10 @@
+import React from 'react';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
-import React from 'react';
+
 
 export async function getStaticProps({ params }) {
     // Add the "await" keyword like this:
